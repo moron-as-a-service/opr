24ccf4213bd76b6f18d25470334dff0051ae1ab0
moron-as-a-service <johannes.steiner@protonmail.com>
2018-09-14 13:01:45 +0200

    added db and first auth bp

--- opr/__init__.py
+++ opr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'proto.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'opr.sqlite'),
     )
 
     if test_config is None:
@@ -27,5 +27,13 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    # initialize db
+    from . import db
+    db.init_app(app)
+
+    # register blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
